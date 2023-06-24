0a713ca2dd9aeff5e85a91dfe2a44a519298b236
robot <robot@umbrella>
2023-06-24 02:01:27 +0000

    Release v0.1.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,1 +1,2 @@
+## v0.1.0
 * Implemented `gorm-driver` for `YDB` with `database/sql` `YDB` driver
--- internal/meta/version.go
+++ internal/meta/version.go
@@ -2,6 +2,6 @@ package meta
 
 const (
 	VersionMajor = "0"
-	VersionMinor = "0"
-	VersionPatch = "5"
+	VersionMinor = "1"
+	VersionPatch = "0"
 )
